bd7510d5b7ea9ccb647928ed05599a0846f60e38
RyBB <ryo180xion@gmail.com>
2018-07-04 11:30:01 +0900

    make dir

---
+++ handson/hands-on3.js
@@ -0,0 +1,18 @@
+// ①まずはお約束の即時関数 + 厳格モード記述
+(function() {
+  'use strict';
+
+  // ② kintone JS APIの記述
+  kintone.events.on('app.record.create.show', function(event) {
+
+    // ③ リクエストボディの記述
+    var body = {
+      app: 293, // アプリIDを記述
+      id: 1 // レコードIDを記述
+    };
+    // ③ kintone REST APIの記述：レコード1件取得
+    kintone.api(kintone.api.url('/k/v1/record'),'GET', body, function(resp) {
+      console.log(resp);
+    });
+  });
+})();
