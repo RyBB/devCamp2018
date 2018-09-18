0fb7a49ffaf1ae9df8f2a3858976c263402ba38e
RyBB <ryo180xion@gmail.com>
2018-09-18 16:54:17 +0900

    refactoring

--- sample/sample3.js
+++ sample/sample3.js
@@ -3,11 +3,12 @@
 スペースフィールド (フィールドコード：space)を配置してください。
 そのスペースフィールドに対してappendでボタンを生成します。
 */
+
 (function() {
   'use strict';
 
   // 郵便番号から住所を取得する関数
-  var getAddress = function () {
+  var getAddress = function() {
     // 関数実行時のフィールド情報を取得する
     var e = kintone.app.record.get();
     var ZIPCODE = e.record['zipcode'].value;
@@ -16,8 +17,9 @@
     var URL = 'http://zipcloud.ibsnet.co.jp/api/search?zipcode=' + ZIPCODE;
 
     // 外部APIを実行する：リクエスト
-    kintone.proxy(URL, 'GET', {}, {}).then(function(resp) {
-      var res = JSON.parse(resp[0])
+    kintone.proxy(URL, 'GET', {}, {})
+    .then(function(resp) {
+      var res = JSON.parse(resp[0]);
       if (!res.results) {
         window.alert('郵便番号を入力してください');
         return;
@@ -28,15 +30,13 @@
 
       // イベントハンドラーがすでに終わっているので意図的にeventを返す
       kintone.app.record.set(e);
-    }, function(err) {
+    })
+    .catch(function(err) {
       window.alert(err);
     });
   };
   // 住所フィールドの編集権限を不可にする + ボタン生成
-  var events = [
-    'app.record.create.show',
-    'app.record.edit.show'
-  ];
+  var events = ['app.record.create.show', 'app.record.edit.show'];
   kintone.events.on(events, function(event) {
     var record = event.record;
     record['address1'].disabled = true;
@@ -45,7 +45,7 @@
     var btn = document.createElement('button');
     btn.textContent = '郵便番号から住所を検索する';
     btn.style.height = '50px';
-    btn.style.marginTop = '10%'
+    btn.style.marginTop = '10%';
     btn.onclick = getAddress;
     kintone.app.record.getSpaceElement('space').appendChild(btn);
     return event;
