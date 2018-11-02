dc34f162596e4358bbcccfca28e5e0ed8640fc13
Ryo Otake <ryo-otake@cybozu.co.jp>
2018-11-02 16:34:02 +0900

    change dir name

--- handson/hands-on2.sh
+++
@@ -1,7 +0,0 @@
-# コマンドプロンプト, ターミナルの方
-curl -X "GET" -H "X-Cybozu-API-Token: YOUR_TOKEN" \
-"https://{subdomain}.cybozu.com/k/v1/records.json?app={XXX}"
-
-# PowerShellの方
-curl.exe -X "GET" -H "X-Cybozu-API-Token: YOUR_TOKEN" \
-"https://{subdomain}.cybozu.com/k/v1/records.json?app={XXX}"
--- handson/hands-on3.js
+++
@@ -1,17 +0,0 @@
-// ①まずはお約束の即時関数 + 厳格モード記述
-(function() {
-  'use strict';
-
-  // ② kintone JS APIの記述
-  kintone.events.on('app.record.create.show', function(event) {
-    // ③ リクエストボディの記述
-    var params = {
-      app: '<アプリID>',
-      id: 1
-    };
-    // ③ kintone REST APIの記述：レコード1件取得
-    kintone.api(kintone.api.url('/k/v1/record'),'GET', params, function(resp) {
-      window.alert(resp.record.text.value);
-    });
-  });
-})();
